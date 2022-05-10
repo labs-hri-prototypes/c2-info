94ae2ef3bee5cdba8a31e49d5284ec9d365e23a8
zomme <zomme@AD01736572.local>
2022-05-10 22:38:20 +0900

    added time exception

--- src/simple.js
+++ src/simple.js
@@ -1,3 +1,38 @@
+const URLSearch = new URLSearchParams(location.search);
+const place = searchParam('place');
+const exception = [
+  {
+    place: 'a2',
+    date: '2022-05-11',
+    start: '16:00',
+    end: '17:00'
+  },
+  {
+    place: 'a2',
+    date: '2022-05-16',
+    start: '15:00',
+    end: '16:00'
+  },
+  {
+    place: 'a2',
+    date: '2022-05-16',
+    start: '17:00',
+    end: '18:00'
+  },
+  {
+    place: 'a2',
+    date: '2022-05-19',
+    start: '16:00',
+    end: '17:00'
+  },
+  {
+    place: 'a3',
+    date: '2022-05-11',
+    start: '17:00',
+    end: '18:00'
+  }
+];
+
 
 window.onload = function(){
   mainFunction();
@@ -12,23 +47,42 @@ function mainFunction() {
 
 function updateTime() {
   let current = new Date();
+  let dateString = stringifyDate(current);
   let startString;
   let endString;
-  let endMinute;
-  if(current.getMinutes() < 30) {
-    startString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:00`;
-    endString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:30`;
-    endMinute = 30;
-  }
-  else {
-    let hour = (current.getHours() + 1) % 24;
-    startString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:30`;
-    endString = `${Math.floor(hour/10)}${hour%10}:00`;
-    endMinute = 60;
-  }
+  let endTime;
+  exception.forEach(slot => {
+    let slotStart = new Date(`${slot.date} ${slot.start}`);
+    let slotEnd = new Date(`${slot.date} ${slot.end}`);
+    if(
+      slot.place = place &&
+      current.getTime() >= slotStart.getTime() &&
+      current.getTime() < slotEnd.getTime()
+    ) {
+      console.log("exceptional time");
+      startString = slot.start;
+      endString = slot.end;
+      endTime = new Date(`${dateString} ${slot.end}`);
+    }
+    else {
+      console.log("default time");
+      if(current.getMinutes() < 30) {
+        startString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:00`;
+        endString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:30`;
+        endTime = new Date(`${dateString} ${endString}`);
+      }
+      else {
+        let hour = (current.getHours() + 1) % 24;
+        startString = `${Math.floor(current.getHours()/10)}${current.getHours()%10}:30`;
+        endString = `${Math.floor(hour/10)}${hour%10}:00`;
+        endTime = new Date(`${dateString} ${endString}`);
+      }
+    }
+  });
 
-  let minuteTillEnd = endMinute - current.getMinutes();
-    ;
+  let minuteTillEnd =
+    Math.floor(endTime.getTime()/60000) -
+    Math.floor(current.getTime()/60000);
   let minuteText = `종료 시간까지 ${minuteTillEnd}분 남았습니다.`;
 
   document.querySelector("#time_current").innerText =
@@ -68,4 +122,18 @@ function stringifyTime(input) {
       input.getMinutes()%10
     }`;
   return time;
+}
+
+function stringifyDate(input) {
+  const date = `${
+      input.getFullYear()
+    }-${Math.floor((input.getMonth()+1)/10)}${
+      (input.getMonth()+1)%10
+    }-${Math.floor(input.getDate()/10)}${
+      input.getDate()%10}`;
+  return date;
+}
+
+function searchParam(key) {
+  return new URLSearchParams(location.search).get(key);
 }
